f74ccb49439e29bf13af02d7c5fe20c395b66e25
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-18 10:04:07 -0400

    Consommation de RSS

---
+++ AgenceSpatiale/NouvelleDAO.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System;
+using System.Net;
+
+
+namespace AgenceSpatiale
+{
+	class NouvelleDAO
+	{
+		public string listerNouvelles(string rss)
+		{
+			Console.WriteLine("RssDAO.listerNouvelles(" + rss + ")");
+			HttpWebRequest requeteNouvelles = (HttpWebRequest)WebRequest.Create(rss);
+			requeteNouvelles.Method = "GET";
+			//requeteNouvelles.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+			requeteNouvelles.UserAgent = "Mozilla Firefox";
+			//ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+			WebResponse reponse = requeteNouvelles.GetResponse();
+			StreamReader lecteur = new StreamReader(reponse.GetResponseStream());
+			return lecteur.ReadToEnd();
+		}
+	}
+}
--- AgenceSpatiale/Program.cs
+++ AgenceSpatiale/Program.cs
@@ -8,8 +8,21 @@ namespace AgenceSpatiale
 		{
 			SeismeDAO seismeDAO;
 			seismeDAO = new SeismeDAO();
-			string xmlSeismeStella = seismeDAO.listerSeismes("Stella");
-			Console.WriteLine(xmlSeismeStella);
+			//string xmlSeismeStella = seismeDAO.listerSeismes("Stella");
+			//Console.WriteLine(xmlSeismeStella);
+
+			//string rssMeteoQuebec = "http://meteo.gc.ca/rss/city/qc-133_f.xml";
+			NouvelleDAO nouvelleDAO = new NouvelleDAO();
+			//string xmlMeteoQuebec = nouvelleDAO.listerNouvelles(rssMeteoQuebec);
+			//Console.WriteLine(xmlMeteoQuebec);
+
+			//string rssBlogCodeur = "http://www.joelonsoftware.com/feed/";
+			//string xmlBlogCodeur = nouvelleDAO.listerNouvelles(rssBlogCodeur);
+			//Console.WriteLine(rssBlogCodeur);
+
+			string rssNouvellesLivres = "http://ici.radio-canada.ca/rss/1000083";
+			string xmlNouvellesLivres = nouvelleDAO.listerNouvelles(rssNouvellesLivres);
+			Console.WriteLine(xmlNouvellesLivres);
 			Console.ReadKey(true);
 		}
 	}
