baabfabeb8d1eeeb8217cee6cc32685218badc18
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-18 08:54:26 -0400

    Factorisation en DAO

--- AgenceSpatiale/Program.cs
+++ AgenceSpatiale/Program.cs
@@ -1,24 +1,16 @@
 ﻿using System;
-using System.Net;
-using System.IO;
-using System.Net.Http;
 
 namespace AgenceSpatiale
 {
 	class Program
 	{
-		protected static string urlSeismesStella = "https://soda.demo.socrata.com/resource/6yvf-kk3n.xml?source=pr&$where=region%20like%20%27%25Stella%25%27";
-
 		static void Main(string[] args)
 		{
-
-			WebRequest requeteSeismesStella = WebRequest.Create(urlSeismesStella);
-			WebResponse reponse = requeteSeismesStella.GetResponse();
-			StreamReader lecteur = new StreamReader(reponse.GetResponseStream());
-			string xmlSeismeStella = lecteur.ReadToEnd();
+			SeismeDAO seismeDAO;
+			seismeDAO = new SeismeDAO();
+			string xmlSeismeStella = seismeDAO.listerSeismes();
 			Console.WriteLine(xmlSeismeStella);
 			Console.ReadKey(true);
-
 		}
 	}
 }
---
+++ AgenceSpatiale/SeismeDAO.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.IO;
+
+namespace AgenceSpatiale
+{
+	class SeismeDAO
+	{
+		public static string URL_SEISME_STELLA = "http://soda.demo.socrata.com/resource/6yvf-kk3n.xml?source=pr&$where=region%20like%20%27%25Stella%25%27";
+
+		public string listerSeismes()
+		{
+			WebRequest requeteSeismesStella = WebRequest.Create(URL_SEISME_STELLA);
+			WebResponse reponse = requeteSeismesStella.GetResponse();
+			StreamReader lecteur = new StreamReader(reponse.GetResponseStream());
+			return lecteur.ReadToEnd();
+		}
+	}
+}
