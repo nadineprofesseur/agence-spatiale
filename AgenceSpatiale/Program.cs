632c61c8d24ed506b5c2fc5a2c4604b41bd9c6c7
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-18 08:24:18 -0400

    Consommation de données de seismes en service web

--- AgenceSpatiale/Program.cs
+++ AgenceSpatiale/Program.cs
@@ -7,10 +7,16 @@ namespace AgenceSpatiale
 {
 	class Program
 	{
+		protected static string urlSeismesStella = "https://soda.demo.socrata.com/resource/6yvf-kk3n.xml?source=pr&$where=region%20like%20%27%25Stella%25%27";
 
 		static void Main(string[] args)
 		{
 
+			WebRequest requeteSeismesStella = WebRequest.Create(urlSeismesStella);
+			WebResponse reponse = requeteSeismesStella.GetResponse();
+			StreamReader lecteur = new StreamReader(reponse.GetResponseStream());
+			string xmlSeismeStella = lecteur.ReadToEnd();
+			Console.WriteLine(xmlSeismeStella);
 			Console.ReadKey(true);
 
 		}
