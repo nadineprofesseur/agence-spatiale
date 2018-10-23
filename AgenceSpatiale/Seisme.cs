e0420600e27add074cbe347f693b0223e26b9ac9
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-23 17:48:22 -0400

    Classe Seisme

---
+++ AgenceSpatiale/Seisme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenceSpatiale
+{
+	class Seisme
+	{
+		public string source { get; set; }
+		public string magnitude { get; set; }
+		public string profondeur { get; set; }
+		public string region { get; set; }
+	}
+}
