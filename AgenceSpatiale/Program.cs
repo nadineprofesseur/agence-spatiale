75e2f340070148a58ad433f455e877579f6e87bb
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-23 16:09:09 -0400

    Testé versus non-testé

--- AgenceSpatiale/Program.cs
+++ AgenceSpatiale/Program.cs
@@ -7,22 +7,27 @@ namespace AgenceSpatiale
 		static void Main(string[] args)
 		{
 			SeismeDAO seismeDAO;
+			NouvelleDAO nouvelleDAO = new NouvelleDAO();
+
+			//FONCTIONNEL
 			seismeDAO = new SeismeDAO();
 			//string xmlSeismeStella = seismeDAO.listerSeismes("Stella");
 			//Console.WriteLine(xmlSeismeStella);
 
-			//string rssMeteoQuebec = "http://meteo.gc.ca/rss/city/qc-133_f.xml";
-			NouvelleDAO nouvelleDAO = new NouvelleDAO();
-			//string xmlMeteoQuebec = nouvelleDAO.listerNouvelles(rssMeteoQuebec);
-			//Console.WriteLine(xmlMeteoQuebec);
+			string rssMeteoQuebec = "http://meteo.gc.ca/rss/city/qc-133_f.xml";
+			string xmlMeteoQuebec = nouvelleDAO.listerNouvelles(rssMeteoQuebec);
+			Console.WriteLine(xmlMeteoQuebec);
+
+			// DISFONCTIONNEL
 
 			//string rssBlogCodeur = "http://www.joelonsoftware.com/feed/";
 			//string xmlBlogCodeur = nouvelleDAO.listerNouvelles(rssBlogCodeur);
 			//Console.WriteLine(rssBlogCodeur);
 
-			string rssNouvellesLivres = "http://ici.radio-canada.ca/rss/1000083";
-			string xmlNouvellesLivres = nouvelleDAO.listerNouvelles(rssNouvellesLivres);
-			Console.WriteLine(xmlNouvellesLivres);
+			//string rssNouvellesLivres = "http://ici.radio-canada.ca/rss/1000083";
+			//string xmlNouvellesLivres = nouvelleDAO.listerNouvelles(rssNouvellesLivres);
+			//Console.WriteLine(xmlNouvellesLivres);
+
 			Console.ReadKey(true);
 		}
 	}
