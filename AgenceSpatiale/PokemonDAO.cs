aa50208efb48a1c8827e479daec1f0c7e14a8802
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-25 09:39:02 -0400

    Parser le json des pokemon - affiche le nombre total

--- AgenceSpatiale/PokemonDAO.cs
+++ AgenceSpatiale/PokemonDAO.cs
@@ -1,13 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net;
 using System.IO;
+using System.Web.Script.Serialization;
+// Références -> Ajouter une référence -> Assemblies -> Framework : System.Web.Extension
+// https://stackoverflow.com/questions/7000811/cannot-find-javascriptserializer-in-net-4-0
+// using System.Collections.Generic;
 
 namespace AgenceSpatiale
 {
 	class PokemonDAO
 	{
-
 		public string listerPokemon()
 		{
 			Console.WriteLine("PokemonDAO.listerPokemon()");
@@ -19,6 +21,15 @@ namespace AgenceSpatiale
 			StreamReader lecteurListePokemon = new StreamReader(reponseListePokemon.GetResponseStream());
 			string json = lecteurListePokemon.ReadToEnd();
 			Console.WriteLine(json);
+
+			// https://docs.microsoft.com/en-us/dotnet/api/system.web.script.serialization.javascriptserializer
+			// https://social.msdn.microsoft.com/Forums/vstudio/en-US/a2e31874-e870-459c-a2a4-2a8d25b20fae/how-to-parse-a-object-using-systemjson?forum=csharpgeneral
+			JavaScriptSerializer parseur = new JavaScriptSerializer();
+			// https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/types/using-type-dynamic
+			dynamic objet = parseur.Deserialize<dynamic>(json);
+			string nombre = objet["count"].ToString();
+
+			Console.WriteLine(nombre + " pokemons recus");
 			return json;
 		}
 	}
