d91476b90b6e8165c62c5be04182f3fda07a1ccd
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-25 09:51:53 -0400

    Modele Pokemon

---
+++ AgenceSpatiale/Pokemon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenceSpatiale
+{
+	class Pokemon
+	{
+		public string nom {get;set;}
+		public int hauteur {get;set;}
+		public int numero { get; set; }
+		public string illustration { get; set; }
+	}
+}
