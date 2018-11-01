eaea434d56d88670a8f108038345ba0c7a17af65
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-11-01 09:29:57 -0400

    Valeur de cryptomonnaie parsées

--- AgenceSpatiale/CryptoMonnaieDAO.cs
+++ AgenceSpatiale/CryptoMonnaieDAO.cs
@@ -8,6 +8,29 @@ namespace AgenceSpatiale
 {
 	class CryptoMonnaieDAO
 	{
+
+		/*
+		     "GLYPH": {
+      "Id": "4530",
+      "Url": "/coins/glyph/overview",
+      "ImageUrl": "/media/19725/glyph.png",
+      "Name": "GLYPH",
+      "Symbol": "GLYPH",
+      "CoinName": "GlyphCoin",
+      "FullName": "GlyphCoin (GLYPH)",
+      "Algorithm": "X11",
+      "ProofType": "PoW/PoS",
+      "FullyPremined": "0",
+      "TotalCoinSupply": "7000000",
+      "BuiltOn": "N/A",
+      "SmartContractAddress": "N/A",
+      "PreMinedValue": "N/A",
+      "TotalCoinsFreeFloat": "N/A",
+      "SortOrder": "126",
+      "Sponsored": false
+    },
+
+			 */
 		public string listerMonnaies()
 		{
 			Console.WriteLine("CryptoMonnaieDAO.listerMonnaies()");
@@ -21,10 +44,18 @@ namespace AgenceSpatiale
 			JavaScriptSerializer parseur = new JavaScriptSerializer();
 			dynamic objet = parseur.Deserialize<dynamic>(json);
 			var lesMonnaies = objet["Data"];
-			foreach (dynamic monnaie in lesMonnaies)
+			foreach (dynamic itemMonnaie in lesMonnaies)
 			{
-				Console.WriteLine(monnaie.ToString());
+				//Console.WriteLine(itemMonnaie.ToString());
 				// Donne : [AXIS, System.Collections.Generic.Dictionary`2[System.String, System.Object]]
+				// Même si on a [truc1, truc2] c'est pas un tableau, c'est un cle => valeur, acces avec .Key & .Value
+				var monnaie = itemMonnaie.Value;
+				var symbole = monnaie["Symbol"];
+				var nom = monnaie["CoinName"];
+				var algorithme = monnaie["Algorithm"];
+				var nombre = monnaie["TotalCoinSupply"];
+				// var illustration = monnaie["ImageUrl"]; KeyNotFoundException
+				Console.WriteLine("Monnaie " + symbole + " : " + nom + "("+nombre+")");
 			}
 
 			return json;
