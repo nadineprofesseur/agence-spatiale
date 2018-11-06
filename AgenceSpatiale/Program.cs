76a387e4ddac447a3d8cbcdc4b1463ea569bf9c3
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-11-06 17:41:42 -0500

    Nouveau DAO - test prêt avant le code de la fonction

--- AgenceSpatiale/Program.cs
+++ AgenceSpatiale/Program.cs
@@ -11,12 +11,17 @@ namespace AgenceSpatiale
 			NouvelleDAO nouvelleDAO = new NouvelleDAO();
 			PokemonDAO pokemonDAO = new PokemonDAO();
 			CryptoMonnaieDAO cryptomonnaieDAO = new CryptoMonnaieDAO();
-			List<CryptoMonnaie> listeMonnaies = cryptomonnaieDAO.listerMonnaies();
+			SalonDAO salonDAO = new SalonDAO();
+
+			string listeSalons = salonDAO.listerSalons();
+			Console.WriteLine(listeSalons);
+
+			/*List<CryptoMonnaie> listeMonnaies = cryptomonnaieDAO.listerMonnaies();
 			foreach (CryptoMonnaie monnaie in listeMonnaies)
 			{
 				Console.WriteLine("Programme principal : " + monnaie.nom);
 			}
-
+			*/
 
 			//FONCTIONNEL
 			/*List<Pokemon> listePokemon = pokemonDAO.listerPokemon();
---
+++ AgenceSpatiale/SalonDAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Liste des salons
+// https://slack.com/api/channels.list?token=xoxp-473953774023-473376698226-472643986400-fb1078e65934f7e2e473d1fa7157a120&pretty=1
+// Messages du salon general
+// https://slack.com/api/channels.history?token=xoxp-473953774023-473376698226-472643986400-fb1078e65934f7e2e473d1fa7157a120&channel=CDXU1P58X&pretty=1
+
+namespace AgenceSpatiale
+{
+	class SalonDAO // Slack
+	{
+		public string listerSalons()
+		{
+			Console.WriteLine("SalonDAO.listerSalons()");
+			string json = "";
+
+			return json;
+		}
+	}
+}
