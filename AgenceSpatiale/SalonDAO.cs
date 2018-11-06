efeac96261aaf9efa94b19b75c2e6bcba0dc7188
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-11-06 17:47:49 -0500

    Récupère liste des salons en json, mais token expire rapidement

--- AgenceSpatiale/SalonDAO.cs
+++ AgenceSpatiale/SalonDAO.cs
@@ -1,8 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 // Liste des salons
 // https://slack.com/api/channels.list?token=xoxp-473953774023-473376698226-472643986400-fb1078e65934f7e2e473d1fa7157a120&pretty=1
@@ -18,6 +18,11 @@ namespace AgenceSpatiale
 			Console.WriteLine("SalonDAO.listerSalons()");
 			string json = "";
 
+			string url = "https://slack.com/api/channels.list?token=xoxp-473953774023-473376698226-472661774848-22e172d773a15e29f55cb0e0980ea05b&pretty=1";
+			WebRequest requetesSalons = WebRequest.Create(url);
+			WebResponse reponse = requetesSalons.GetResponse();
+			StreamReader lecteur = new StreamReader(reponse.GetResponseStream());
+			json = lecteur.ReadToEnd();
 			return json;
 		}
 	}
